eb034c97b0cfb065587c222e8c57583bfe0c7320
AurisZoz <121929818+AurisZoz@users.noreply.github.com>
2023-04-25 19:32:12 +0300

    Create projektaslaravel.sql

---
+++ projektaslaravel.sql
@@ -0,0 +1,183 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 25, 2023 at 06:29 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `projektaslaravel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `automobiliais`
+--
+
+CREATE TABLE `automobiliais` (
+  `id` int(11) NOT NULL,
+  `automobilio_id` int(11) NOT NULL,
+  `marke` text NOT NULL,
+  `modelis` text NOT NULL,
+  `numeris` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `automobiliais`
+--
+
+INSERT INTO `automobiliais` (`id`, `automobilio_id`, `marke`, `modelis`, `numeris`) VALUES
+(2, 1, 'Audi', 'Q5', 'QEW782'),
+(3, 2, 'Tesla', 'Roadster', 'PHJ785'),
+(3, 3, 'Porsche', 'Panamera', 'MNH952'),
+(3, 4, 'Ford', 'Ka', 'HGF478'),
+(4, 5, 'Daewoo', 'Kalos', 'NGF555'),
+(5, 6, 'Chevrolet', 'Silverado', 'VDF254'),
+(6, 7, 'Mitsubishi', 'Outlander', 'NGF411'),
+(6, 8, 'Audi', 'RS5', 'GRE659'),
+(7, 9, 'Mini', 'Countryman', 'RTE012'),
+(8, 10, 'Hyundai', 'i20', 'MKL000'),
+(8, 11, 'Kia', 'Forte', 'VV753'),
+(8, 12, 'Acura', 'MDX', 'LLO548'),
+(9, 13, 'BMW', 'X1', 'POH444'),
+(10, 14, 'Peugeot', '208', 'NFR228'),
+(1, 15, 'Ford', 'Galaxy', 'TRE008');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `savininkais`
+--
+
+CREATE TABLE `savininkais` (
+  `id` int(11) NOT NULL,
+  `vardas` text NOT NULL,
+  `pavarde` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `savininkais`
+--
+
+INSERT INTO `savininkais` (`id`, `vardas`, `pavarde`) VALUES
+(1, 'Jonas', 'Sinkevičius'),
+(2, 'Ahmet', 'Tadur'),
+(3, 'Jaxson', 'Ruiz'),
+(4, 'Garfield', 'Key'),
+(5, 'Ruth', 'Leon'),
+(6, 'Jazmin', 'Villa'),
+(7, 'Rahul', 'Sanders'),
+(8, 'Luqman', 'Burke'),
+(9, 'Robert', 'Vaughn'),
+(10, 'Priya', 'Mcintyre');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `automobiliais`
+--
+ALTER TABLE `automobiliais`
+  ADD PRIMARY KEY (`automobilio_id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `automobiliais`
+--
+ALTER TABLE `automobiliais`
+  MODIFY `automobilio_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
